b1c64bbae67d31e0adafb7774087f62c2b0b6f77
bradgroznik <brad@groznikpr.com>
2025-05-15 12:28:02 -0400

    Add sparkling animation to homepage emphasis text and change primary color to neon orange

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,12 +15,49 @@ module.exports = {
       },
       // custom color palette for branding, see https://tailwindcss.com/docs/customizing-colors
       colors: {
-        primary: colors.lime,
+        primary: {
+          50: '#fff9eb',
+          100: '#ffe7c7',
+          200: '#ffd28c',
+          300: '#ffb951',
+          400: '#ff9f24',
+          500: '#ff8c0d',
+          600: '#ff6b00',
+          700: '#cc4e02',
+          800: '#a13d0b',
+          900: '#82340c'
+        },
         secondary: colors.black
       },
       keyframes: {
         flashfade: { "0%, 100%": { opacity: "0" }, "5%, 80%": { opacity: "1" } },
+        sparkle: {
+          '0%, 100%': {
+            opacity: '1',
+            transform: 'scale(1) rotate(0deg)',
+            textShadow: '0 0 7px #ff8c0d, 0 0 10px #ff8c0d, 0 0 21px #ff8c0d'
+          },
+          '25%': {
+            opacity: '0.8',
+            transform: 'scale(1.1) rotate(2deg)',
+            textShadow: '0 0 10px #ff8c0d, 0 0 20px #ff8c0d, 0 0 30px #ff8c0d'
+          },
+          '50%': {
+            opacity: '1',
+            transform: 'scale(1.2) rotate(-2deg)',
+            textShadow: '0 0 15px #ff8c0d, 0 0 25px #ff8c0d, 0 0 40px #ff8c0d'
+          },
+          '75%': {
+            opacity: '0.8',
+            transform: 'scale(1.1) rotate(1deg)',
+            textShadow: '0 0 10px #ff8c0d, 0 0 20px #ff8c0d, 0 0 30px #ff8c0d'
+          }
+        }
       },
+      animation: {
+        flashfade: 'flashfade 5s ease-in-out',
+        sparkle: 'sparkle 2s ease-in-out infinite'
+      }
     },
   },
   plugins: [
